3499ea2ddf45cfb306653c34343840f9ce42a273
Swapnil-Pisal <swapnilpisal193@gmail.com>
2026-02-09 10:32:17 +0530

    Modues use another file

---
+++ Foundational Python Concepts/Modules/calulater.py
@@ -0,0 +1,22 @@
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def mul(a,b):
+    print(a*b)
+def div(a,b):
+    print(a/b)
+def mod(a,b):
+    print(a%b)
+def pow(a,b):
+    print(a**b)
+def sqrt(a):
+    print(a**0.5)
+
+add(10,20)
+sub(10,20)
+mul(10,20)
+div(10,20)
+mod(10,20)
+pow(10,20)
+sqrt(10)
---
+++ Foundational Python Concepts/Modules/module use.py
@@ -0,0 +1,9 @@
+import calulater
+
+print(calulater.add(10,20))
+print(calulater.sub(10,20))
+print(calulater.mul(10,20))
+print(calulater.div(10,20))
+print(calulater.mod(10,20))
+print(calulater.pow(10,20))
+print(calulater.sqrt(10))
